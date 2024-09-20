d1f5c144d1ee6e92e5e1cd95b81113dc760a9f47
kigawa01 <contact@kigawa.net>
2024-09-20 06:31:47 +0000

    wip

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 ##################################################
-set -ue
+set -e
 ##################################################
 
-KUTIL_HOME=$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit 1 ; pwd)
+if [ -z "$KUTIL_HOME" ]; then
+  KUTIL_HOME=$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit 1 ; pwd)
 
-for file in "$KUTIL_HOME"/bin/* ; do
-  echo chmod "$file"
-  chmod +x "$file"
-done
-echo "source $KUTIL_HOME/.bashrc" >> "$HOME/.bashrc"
-echo "export PATH=\"${KUTIL_HOME}/bin:\$PATH\"" >> "$HOME/.bashrc"
-# shellcheck disable=SC1012
-echo -e "run \n source \"$HOME/.bashrc\""
+  for file in "$KUTIL_HOME"/bin/* ; do
+    echo chmod "$file"
+    chmod +x "$file"
+  done
+  echo "source $KUTIL_HOME/.bashrc" >> "$HOME/.bashrc"
+  echo "export PATH=\"${KUTIL_HOME}/bin:\$PATH\"" >> "$HOME/.bashrc"
+  # shellcheck disable=SC1012
+  echo -e "run \n source \"$HOME/.bashrc\""
+fi
+
+if [ -z "$KUTIL_SSH_KEY" ]; then
+    echo type ssh key path
+    read -r KUTIL_SSH_KEY
+  echo "export KUTIL_SSH_KEY=\"${KUTIL_SSH_KEY}\"" >> "$HOME/.bashrc"
+fi
